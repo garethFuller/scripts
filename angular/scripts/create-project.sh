20c34284a8c4394a77955138fe2d43abe6348bcb
garethFuller <gareth-fuller@hotmail.co.uk>
2015-05-24 22:47:11 +0200

    Adding more to create project task

--- angular/scripts/create-project.sh
+++ angular/scripts/create-project.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
 function createProject {
-  appName=$1
+
+  echo "-------------------------"
   echo "cerating project $1"
-  cp -r "$scriptPath"/../base/index.html .
-  exit 1
+  echo "-------------------------"
+
+  # copy the index file
+  cp "$scriptPath"/../base/index.html .
+
+  # create the project structure
+  mkdir scripts
+  mkdir scripts/modules
+
+  # copy over the app, not including any modules
+  cp "$scriptPath"/../base/app.js ./scripts
+  cp -r "$scriptPath"/../base/app ./scripts/modules/
+
+  # copy over the package json
+  cp "$scriptPath"/../base/package.json .
+
+  # Replace any app name constants in the copied files
+  find ./ -type f -exec sed -i "" "s/APPNAME/$1/g" {} \;
+
+  # install the node modules
+  npm install
+
+  echo "-------------------------"
+  echo "done creating project"
+  echo "-------------------------"
 }
