789236db7dcf0cb33f2021ebe3ea82401abfc886
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2016-03-28 20:38:12 +0200

    Remove the route and import from the app

--- angular-2/scripts/remove-item.sh
+++ angular-2/scripts/remove-item.sh
@@ -14,11 +14,22 @@ function removeItem {
     if [ "$name" == "$kebabCase" ] ; then
       # remove the whole folder
       rm -r $f
+
+      # if the item is a component we need to remove it from the app.ts routes
+      if [ $type == 'components' ]; then
+        ucFirst=$( ucFirst $2 )
+        ucFirstComponentName=$ucFirst"Component"
+        sed -i "" "/import {$ucFirstComponentName}\ from\ '\.\.\/$kebabCase\/index'\;/d" "app/components/app/app.ts"
+        sed -i "" "/{path\:'\/$kebabCase'\,\ name\:\ '$ucFirst'\,\ component\:\ $ucFirstComponentName}\,/d" "app/components/app/app.ts"
+      fi
+
       log "$1 removed."
       removed="yup"
+
     fi
 
   done
+
 
   if [ "$removed" != "yup" ] ; then
     log "Could not find item $1 $2" "error"
