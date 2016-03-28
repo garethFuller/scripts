662c8c977ac88557e93d932efe1891c5143e9889
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2016-03-28 18:17:38 +0200

    Remove item working

--- angular-2/scripts/remove-item.sh
+++ angular-2/scripts/remove-item.sh
@@ -2,58 +2,25 @@
 
 function removeItem {
 
-  modules=()
-  for f in ./src/scripts/modules/*; do
-    # remove the path and get the module name
-    module=${f#./*modules/}
+  # Make the type plural
+  type="$1"s
 
-    kebabCase=$( kebabCase $1 )
+  for f in ./app/"$type"/*; do
+    # remove the path and get the item name
+    name=${f#./*"$type"/}
+    kebabCase=$( kebabCase $2 )
 
-    # check for the module by name
-    if [ "$module" == "$kebabCase" ] ; then
-      # remove the whole module
+    # check for the item by name
+    if [ "$name" == "$kebabCase" ] ; then
+      # remove the whole folder
       rm -r $f
-
-      # remove the module ref in the app
-      sed -i "" "/require('..\/$kebabCase').name/d" ./src/scripts/modules/app/index.js
-
-      # remove the last , of the module list if it was the last one
-      stripExtraComma
-
-      log "Module $module removed."
-
+      log "$1 removed."
       removed="yup"
-    fi
-
-    if [ "$2" ] ; then
-
-      upperName=$( ucFirst "$2" )
-      name="$1"
-
-      # now check the files inside that folder to see if there is a file with
-      # the same name as the item we want to remove
-      if [ -e "$f/$name$upperName.js" ] ; then
-
-        # remove the file
-        rm "$f/$name$upperName.js"
-
-        # remove the includes in the module
-        sed -i "" "/require('.\/$name$upperName.js');/d" "$f/index.js"
-
-        log "Item removed from within module $module"
-
-        removed="yup"
-      fi
-    fi
-
-    # check for HTML files, as directives create the templates
-    if [ -e "$f/$kebabCase.html" ] ; then
-      rm "$f/$kebabCase.html"
     fi
 
   done
 
   if [ "$removed" != "yup" ] ; then
-    log "Could not find item $1" "error"
+    log "Could not find item $1 $2" "error"
   fi
 }
