553a5af17345a7dfb1c6eabbd8500039d8422a3b
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2016-03-28 19:40:14 +0200

    Adds the import for the component on router

--- angular-2/scripts/add-route.sh
+++ angular-2/scripts/add-route.sh
@@ -7,4 +7,7 @@ function addRoute {
   # -i replce inline (had to be seperate on osx)
   perl -0777 -i -pe "s/(\@RouteConfig\(\[(.|\n)*)(.*)?(\n\]\))/\1\3\n\ \ \{path\:'\/$1'\, name\: '$2'\, component\: $2Component\}\,\4/g" ./app/components/app/app.ts
 
+
+  # insert the component injection into the app also
+  perl -0777 -i -pe "s/(import.*\n)\n/\1import\ \{$2Component\}\ from\ '\.\.\/$1\/index'\;\n\n/g" ./app/components/app/app.ts
 }
