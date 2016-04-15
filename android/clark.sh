553074db76c84badbd43e3c6bddfca04461c19a0
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2016-04-15 09:28:52 +0200

    Add build for simulator to clark function

--- android/clark.sh
+++ android/clark.sh
@@ -4,7 +4,7 @@ function clark {
   customPath="/Users/garethfuller/Development/AppAndroid/AppAndroid"
   replace="\/Users\/garethfuller\/Development\/AppAndroid\/AppAndroid"
   url="http:\/\/192\.168\.16\.15:3000"
-  devUrl="https:\/\/clark:clarkkent@dev\.clark\.de"
+  devUrl="https:\/\/(.*)?@dev\.clark\.de"
   devUrlTwo="https:\/\/dev\.clark\.de"
 
   # Swap the paths out in settings gradle file
@@ -13,6 +13,10 @@ function clark {
   # Swap the dev URLs for the local ones
   replaceIn "$customPath/AndroidApp/src/main/java/de/clark/activities/LoginActivity.java" $devUrl $url
   replaceIn "$customPath/AndroidApp/src/main/java/de/clark/activities/LoginActivity.java" $devUrlTwo $url
+
+  # Build the app
+  cd ~/Development/AppAndroid/AppAndroid
+  ./AndroidApp/gradlew installDebug -PasX86 && adb shell am start -n de.clark/de.clark.activities.SplashActivity
 
   echo 'Done'
 }
